7d90bcdd53d06468919b0258b6ae8d33c9e8275c
Chelsi Nolasco <chelsinolasco@MacBook-Pro-2.local>
2019-03-10 11:54:34 +0100

    name changes

--- playground/src/App.js
+++ playground/src/App.js
@@ -5,18 +5,13 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className="ChelsiApp">
+        <header className="ChelsiHeader">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
         </header>
